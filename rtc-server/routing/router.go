3f77d6086151da113f3766a76e6d451fa3beda23
SergeyShpak <sshpak92@gmail.com>
2018-07-21 13:56:06 +0200

    feat(routing): add methods to routing

--- rtc-server/routing/router.go
+++ rtc-server/routing/router.go
@@ -7,14 +7,21 @@ import (
 
 var defaultHandler http.Handler = http.HandlerFunc(handleUnknown)
 
+type HttpMethod string
+
+const (
+	HttpMethodGet  HttpMethod = "GET"
+	HttpMethodPost HttpMethod = "POST"
+)
+
 type Router struct {
-	routes         map[string]*Route
+	routes         map[string](map[HttpMethod]*Route)
 	defaultHandler http.Handler
 }
 
 func NewRouter() *Router {
 	return &Router{
-		routes: make(map[string]*Route),
+		routes: make(map[string](map[HttpMethod]*Route)),
 	}
 }
 
@@ -22,20 +29,25 @@ func (r *Router) SetDefaultHandler(h http.Handler) {
 	r.defaultHandler = h
 }
 
-func (r *Router) newRoute(path string) *Route {
+func (r *Router) newRoute(path string, method HttpMethod) *Route {
 	route := &Route{
 		path: path,
 	}
-	r.routes[path] = route
+	routesMap, ok := r.routes[path]
+	if !ok {
+		r.routes[path] = make(map[HttpMethod]*Route)
+		routesMap = r.routes[path]
+	}
+	routesMap[method] = route
 	return route
 }
 
-func (r *Router) HandleFunc(path string, f func(http.ResponseWriter, *http.Request)) *Route {
-	return r.newRoute(path).Handler(f)
+func (r *Router) HandleFunc(path string, method HttpMethod, f func(http.ResponseWriter, *http.Request)) *Route {
+	return r.newRoute(path, method).Handler(f)
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	handler := r.getHandler(req.URL.Path)
+	handler := r.getHandler(req.URL.Path, HttpMethod(req.Method))
 	if handler == nil {
 		defaultHandler.ServeHTTP(w, req)
 		return
@@ -43,9 +55,13 @@ func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	handler.ServeHTTP(w, req)
 }
 
-func (r *Router) getHandler(path string) http.Handler {
-	route := r.routes[path]
-	if route == nil {
+func (r *Router) getHandler(path string, method HttpMethod) http.Handler {
+	routesMap, ok := r.routes[path]
+	if !ok || routesMap == nil {
+		return nil
+	}
+	route, ok := routesMap[method]
+	if !ok || route == nil {
 		return nil
 	}
 	return route.handler
--- rtc-server/routing/router_test.go
+++ rtc-server/routing/router_test.go
@@ -10,22 +10,27 @@ func TestRouter_getHandler(t *testing.T) {
 	t.Parallel()
 	cases := []struct {
 		path    string
+		method  HttpMethod
 		handler func(http.ResponseWriter, *http.Request)
 	}{
 		{
 			path:    "/simple",
+			method:  HttpMethodGet,
 			handler: func(w http.ResponseWriter, r *http.Request) {},
 		},
 		{
 			path:    "/two/slashes",
+			method:  HttpMethodGet,
 			handler: func(w http.ResponseWriter, r *http.Request) {},
 		},
 		{
 			path:    "no_head_slash",
+			method:  HttpMethodGet,
 			handler: func(w http.ResponseWriter, r *http.Request) {},
 		},
 		{
 			path:    "complex/no_head_slash",
+			method:  HttpMethodGet,
 			handler: func(w http.ResponseWriter, r *http.Request) {},
 		},
 	}
@@ -34,12 +39,12 @@ func TestRouter_getHandler(t *testing.T) {
 		tc := tc
 		t.Run(fmt.Sprintf("TestRouter_getHandler cases #%d", i), func(t *testing.T) {
 			r := NewRouter()
-			h := r.getHandler(tc.path)
+			h := r.getHandler(tc.path, tc.method)
 			if h != nil {
 				t.Fatalf("router has been just initialized, did not expect to get a handler")
 			}
-			r.HandleFunc(tc.path, tc.handler)
-			h = r.getHandler(tc.path)
+			r.HandleFunc(tc.path, tc.method, tc.handler)
+			h = r.getHandler(tc.path, tc.method)
 			if h == nil {
 				t.Fatalf("path %s should have been handled, but getHandler returned nil", tc.path)
 			}
@@ -47,22 +52,27 @@ func TestRouter_getHandler(t *testing.T) {
 	}
 	noHandlerCases := []struct {
 		path    string
+		method  HttpMethod
 		handler func(http.ResponseWriter, *http.Request)
 	}{
 		{
 			path:    "/simple",
+			method:  HttpMethodGet,
 			handler: nil,
 		},
 		{
 			path:    "/two/slashes",
+			method:  HttpMethodGet,
 			handler: nil,
 		},
 		{
 			path:    "no_head_slash",
+			method:  HttpMethodGet,
 			handler: nil,
 		},
 		{
 			path:    "complex/no_head_slash",
+			method:  HttpMethodGet,
 			handler: nil,
 		},
 	}
@@ -72,12 +82,12 @@ func TestRouter_getHandler(t *testing.T) {
 		tc := tc
 		t.Run(fmt.Sprintf("TestRouter_getHandler noHandlerCases #%d", i), func(t *testing.T) {
 			r := NewRouter()
-			h := r.getHandler(tc.path)
+			h := r.getHandler(tc.path, tc.method)
 			if h != nil {
 				t.Fatalf("router has been just initialized, did not expect to get a handler")
 			}
-			r.HandleFunc(tc.path, tc.handler)
-			h = r.getHandler(tc.path)
+			r.HandleFunc(tc.path, tc.method, tc.handler)
+			h = r.getHandler(tc.path, tc.method)
 			if h == nil {
 				t.Fatalf("path %s should be assoicated with a nil handler, but getHandler returned an http.Handler", tc.path)
 			}
