718ba5c134c779e55806c5f5fd8e7aea08d78b5a
SergeyShpak <sshpak92@gmail.com>
2018-07-26 00:05:35 +0200

    reafctor(rtc-server): remove dead code

--- rtc-server/main.go
+++ rtc-server/main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/SergeyShpak/ReallyTinyChat/rtc-server/cors"
 	"github.com/SergeyShpak/ReallyTinyChat/rtc-server/handlers"
 )
 
@@ -13,29 +12,9 @@ func main() {
 }
 
 func run() {
-	//srv := getDefaultServer(nil)
-	//http.Handle("/", )
 	http.HandleFunc("/conn", handlers.Connect)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-func getDefaultServer(r http.Handler) *http.Server {
-	corsHandler := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
-		AllowedHeaders: []string{
-			"content-type",
-			"cache-control",
-		},
-		ExposedHeaders: []string{
-			"content-type",
-		},
-	})
-	return &http.Server{
-		Handler: corsHandler.Handler(r),
-		Addr:    ":8080",
 	}
 }
